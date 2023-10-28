e0214a1b632ab03442c2255b5b61d17272ea95fe
Ross-A2208 <109631531+Ross-A2208@users.noreply.github.com>
2023-10-28 18:08:16 +0200

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -28,31 +28,32 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
-  //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-  if (! username || !password) {
-    return res.status(404).json({ message: "ERROR SIGNING IN" });
-  }
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
 
-  if (authenticatedUser(username, password)) {
-    let accessToken = jwt.sign(
-      {
-        data: password,
-      },
-      "access",
-      { expiresIn: 60 * 60 }
-    );
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign(
+            {
+                data: password,
+            },
+            "access",
+            { expiresIn: 60 * 60 }
+        );
 
-    req.session.authorization = {
-      accessToken,
-      username,
-    };
-    return res.status(200).send("User successfully logged in");
- } else {
-          return res.status(208).send("Invalid Login. Check username and password");
-  }
+        req.session.authorization = {
+            accessToken,
+            username,
+        };
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res
+            .status(208)
+            .json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 //return res.status(300).json({ message: "Yet to be implemented" });
